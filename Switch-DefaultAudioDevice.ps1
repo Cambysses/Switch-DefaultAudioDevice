d19aba640cf5b8e46dec9e40f42a27cf55dbef22
Cambysses <selxxa@gmail.com>
2020-02-08 17:07:35 -0400

    clean up

--- Switch-DefaultAudioDevice.ps1
+++ Switch-DefaultAudioDevice.ps1
@@ -1,20 +1,22 @@
 ﻿Import-Module AudioDeviceCmdlets
 
+function Switch-DefaultAudioDevice
+{
+    param
+    (
+        $CurrentDevice,
+        $NewDevice
+    )
+
+    if ((Get-AudioDevice -ID $CurrentDevice | Select-Object -ExpandProperty "Default") -eq "True")
+    {
+        Set-AudioDevice -ID $NewDevice
+        return $true
+    }
+}
+
 $Speakers = "{0.0.0.00000000}.{f4659798-580d-4e58-a1f0-e2ec426740b9}"
 $Headphones = "{0.0.0.00000000}.{72b960aa-f4ee-4966-a297-7f1f649921c9}"
 
-# Check if Speakers are default.
-if ((Get-AudioDevice -ID $Speakers | Select-Object -ExpandProperty "Default") -eq "True")
-{
-    Write-Host "Speakers are default. Switching to headphones."
-    Set-AudioDevice -ID $Headphones
-    break
-}
-
-# Check if headphones are default.
-if ((Get-AudioDevice -ID $Headphones | Select-Object -ExpandProperty "Default") -eq "True")
-{
-    Write-Host "Headphones are default. Switching to speakers."
-    Set-AudioDevice -ID $Speakers
-    break
-}
+if (Switch-DefaultAudioDevice -CurrentDevice $Speakers -NewDevice $Headphones){return}
+if (Switch-DefaultAudioDevice -CurrentDevice $Headphones -NewDevice $Speakers){return}
